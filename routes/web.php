967b77ab95e5fb9a91d3352bf0155ab8fbfd0f0d
EC2 Default User <ec2-user@ip-172-31-27-231.ap-northeast-1.compute.internal>
2021-01-08 23:46:28 +0000

    cart4 add

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('users/carts', 'CartController@index')->name('carts.index');
 
 Route::post('users/carts', 'CartController@store')->name('carts.store');
 
+Route::delete('users/carts', 'CartController@destroy')->name('carts.destroy');
+
 Route::get('users/mypage', 'UserController@mypage')->name('mypage');
 
 Route::get('users/mypage/edit', 'UserController@edit')->name('mypage.edit');
